90889bc58e0ef28a10be1c3e357bf54e8aab2761
clueking1 <wcoan300@gmail.com>
2020-07-10 10:14:02 -0400

    get tables is calling and sending to client side

--- app/controllers/get_table_controller.rb
+++ app/controllers/get_table_controller.rb
@@ -1,4 +1,7 @@
 class GetTableController < ApplicationController
   def tables
+    sql = "SELECT * FROM tables"
+    result = ActiveRecord::Base.connection.execute(sql)
+    render json: result
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'get_name/names'
-  get 'create_name/newName'
-  get 'create_table/newTable'
-  get 'get_table/table'
+  #get 'get_name/names'
+  get 'api/tables' => 'get_table#tables'
+  
+  #get 'create_name/newName'
+  #get 'create_table/newTable'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
