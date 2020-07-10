82bd32a4475ad22e89a95b9b729ac4a699f4afec
clueking1 <wcoan300@gmail.com>
2020-07-10 09:33:42 -0400

    migrated two tables to get their schemas

---
+++ db/migrate/20200710133138_create_baby_names.rb
@@ -0,0 +1,10 @@
+class CreateBabyNames < ActiveRecord::Migration[6.0]
+  def change
+    create_table :baby_names do |t|
+      t.string :name
+      t.string :table
+
+      t.timestamps
+    end
+  end
+end
